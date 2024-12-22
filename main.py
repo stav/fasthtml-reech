4e4402231472b67e786d1c6b7f575b30f4ac1ac5
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-22 13:51:25 -0500

    routes

--- main.py
+++ main.py
@@ -1,51 +1,10 @@
-# ruff: noqa: F405, F403, F811
+from fasthtml.common import serve, fast_app
 
-from fasthtml.common import *
-
-from auth import basic_auth, get_login, get_register, post_login, post_register
-from components import Dashboard, Logout, Reach, styles
-from db import users
+from components import styles
+from routes import route
 
 app, rt = fast_app(live=True, debug=True, hdrs=[styles])
 
-User = users.dataclass()
-
-
-@rt("/register")
-def get():
-    return get_register()
-
-
-@rt("/register")
-def post(email: str, password: str):
-    return post_register(email, password)
-
-
-@rt("/login")
-def get():
-    return get_login()
-
-
-@rt("/login")
-def post(session, email: str, password: str):
-    return post_login(session, email, password)
-
-
-@rt("/dashboard")
-@basic_auth
-def get(session):
-    return Dashboard()
-
-
-@rt("/reach")
-@basic_auth
-def post(session, url: str):
-    return Reach(url)
-
-
-@rt("/logout")
-def post(session):
-    return Logout(session)
-
+route(rt)
 
 serve()
