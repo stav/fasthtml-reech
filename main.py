6658bb37b2b1d1b3101ae7a60e694b7d43c38f29
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-22 23:10:49 -0500

    chart actual data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fasthtml.common import serve, fast_app
+from fh_altair import altair_headers
 
 from components import styles
 from routes import route
 
-app, rt = fast_app(live=True, debug=True, hdrs=[styles])
+app, rt = fast_app(live=True, debug=True, hdrs=[styles, altair_headers])
 
 route(rt)
 
--- reach.py
+++ reach.py
@@ -3,8 +3,27 @@
 import csv
 import requests
 
+# import numpy as np
+import pandas as pd
+import altair as alt
+
 from io import StringIO
 from fasthtml.common import *
+from fh_altair import altair2fasthtml
+
+
+def generate_chart(rows):
+    months_years = [f"{row[0]} {row[1]}" for row in rows]
+    current_values = [float(row[2]) for row in rows]
+
+    pltr = pd.DataFrame({"x": months_years, "y": current_values})
+    chart = (
+        alt.Chart(pltr)
+        .mark_line()
+        .encode(y="y", x=alt.X("x", sort=None))
+        .properties(width=400, height=200)
+    )
+    return altair2fasthtml(chart)
 
 
 def Reach(url: str):
@@ -30,4 +49,7 @@ def Reach(url: str):
         except (AssertionError, ValueError):
             print("Invalid data:", row)
 
-    return Card(Pre(rows), header=A(url, href=url))
+    card = Card(Pre(rows), header=A(url, href=url))
+    chart = Div(generate_chart(rows))
+
+    return Div(card, chart)
